734d4aa79f9cf171a74f3e96b4242a24cc623dfb
ian-bravo <bravo.ian@gmail.com>
2023-09-01 10:32:30 -0700

    pass test 1 for the venusAgeConverter method

--- src/js/age.js
+++ src/js/age.js
@@ -4,10 +4,10 @@ export default class Age {
   }
 
   mercuryAgeConverter() {
-    this.ageMercury = this.ageEarth * 0.24
+    this.ageMercury = this.ageEarth * 0.24;
   }
 
   venusAgeConverter() {
-    
+    this.ageVenus = this.ageEarth * 0.62;
   }
 }
